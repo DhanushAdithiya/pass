a0ad0f724da277a3f4189b9cb1ffce273f708906
DhanushAdithiya <k.b.dhanush02@gmail.com>
2024-04-28 16:17:26 +0530

    added support for chaging default folder location

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::{Parser, Subcommand};
+use dirs;
+use std::fs;
+use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
 #[command(name = "Password Manger")]
@@ -14,12 +17,33 @@ enum Commands {
     Init {},
 }
 
+fn create_dir(folder_path: PathBuf) {
+    if folder_path.exists() {
+        println!("Directory already exists {:?}", folder_path);
+    } else {
+        fs::create_dir_all(folder_path)
+            .expect("could not create folder, please check permissions.");
+        println!("Passwords folder successfully created");
+    }
+}
+
 fn main() {
-    let mut folder_path = "~/.passwords";
+    let mut home_folder = dirs::home_dir().unwrap();
     let args = PasswordManager::parse();
 
     match args.command {
-        Commands::Store { folder_path } => todo!(),
-        Commands::Init {} => todo!(),
+        Commands::Store { folder_path } => {
+            if folder_path.contains("~") {
+                let (_, path) = folder_path.split_once("~/").unwrap();
+                home_folder.push(&path);
+                create_dir(home_folder);
+            } else {
+                create_dir(folder_path.into());
+            }
+        }
+        Commands::Init {} => {
+            home_folder.push(".passwords");
+            create_dir(home_folder);
+        }
     }
 }

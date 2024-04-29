30d8a0c65ff434523ddf14c09929ce1ed015228b
DhanushAdithiya <k.b.dhanush02@gmail.com>
2024-04-29 16:46:19 +0530

    removed option to change password location, added insert password

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,13 @@ struct PasswordManager {
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-    Store { folder_path: String },
+    //Store { folder_path: String },
     Init {},
+    Add { website: String },
+    Modify { website: String },
+    Delete { website: String },
+    Get { website: String },
+    List {},
 }
 
 fn create_dir(folder_path: PathBuf) {
@@ -29,21 +34,51 @@ fn create_dir(folder_path: PathBuf) {
 
 fn main() {
     let mut home_folder = dirs::home_dir().unwrap();
+    home_folder.push(".passwords");
     let args = PasswordManager::parse();
 
     match args.command {
-        Commands::Store { folder_path } => {
-            if folder_path.contains("~") {
-                let (_, path) = folder_path.split_once("~/").unwrap();
-                home_folder.push(&path);
-                create_dir(home_folder);
-            } else {
-                create_dir(folder_path.into());
-            }
-        }
         Commands::Init {} => {
-            home_folder.push(".passwords");
+            // TODO: add a "." flag to initialize in the current dir
             create_dir(home_folder);
         }
+        Commands::Add { mut website } => {
+            website.push_str(".txt");
+
+            let mut path = PathBuf::new();
+            path.push(home_folder);
+            path.push(Into::<std::path::PathBuf>::into(website));
+
+            let f = fs::create_dir_all(&path.parent().unwrap());
+            if f.is_ok() {
+                println!("File created");
+
+                let mut password = String::new();
+                println!("Please input your password");
+                std::io::stdin()
+                    .read_line(&mut password)
+                    .expect("no user input provided");
+
+                let mut password_retype = String::new();
+                println!("Please confirm password");
+                std::io::stdin()
+                    .read_line(&mut password_retype)
+                    .expect("no user input provided");
+
+                if password == password_retype {
+                    println!("{path:?}");
+                    fs::write(path, password).expect("could not write to file");
+                    println!("Password Saved Sucessfully");
+                } else {
+                    eprintln!("Passwords do not match!");
+                }
+            } else {
+                println!("Could not create a file, pleas check folder permissions");
+            }
+        }
+        Commands::Modify { website } => todo!(),
+        Commands::Delete { website } => todo!(),
+        Commands::Get { website } => todo!(),
+        Commands::List {} => todo!(),
     }
 }

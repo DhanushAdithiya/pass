c5d9e76355682ac608118468b5a2191c0b28e55f
DhanushAdithiya <k.b.dhanush02@gmail.com>
2024-05-10 10:07:37 +0530

    added password encryption and decryption

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use aes::cipher::{block_padding::Pkcs7, BlockDecryptMut, BlockEncryptMut, KeyIvInit};
+use base64::{engine::general_purpose, Engine as _};
 use clap::{Parser, Subcommand};
 use dirs;
 use std::fs;
 use std::io::prelude::*;
 use std::path::PathBuf;
+
+type Aes128CbcEnc = cbc::Encryptor<aes::Aes128>;
+type Aes128CbcDec = cbc::Decryptor<aes::Aes128>;
 
 #[derive(Debug, Parser)]
 #[command(name = "Password Manger")]
@@ -80,17 +85,47 @@ fn read_input(message: &str) -> String {
         .read_line(&mut op)
         .expect("no user input provided");
 
-    return op;
+    return op.trim_end().to_string();
+}
+
+fn encrypt_password<'a>(key: &'a str, plaintext: &'a str) -> Vec<u8> {
+    let iv = [0u8; 16];
+
+    let buf_len = (plaintext.len() + 16) - (plaintext.len() % 16);
+
+    let mut buf = vec![0; buf_len];
+    let pt_len = plaintext.len();
+    buf[..pt_len].copy_from_slice(plaintext.as_bytes());
+    let ct = Aes128CbcEnc::new(key.as_bytes().into(), &iv.into())
+        .encrypt_padded_mut::<Pkcs7>(&mut buf, pt_len)
+        .unwrap();
+
+    return ct.to_owned();
+}
+fn decrypt_password(mut buf: Vec<u8>, key: &str) -> String {
+    let iv = [0u8; 16];
+
+    let pt = Aes128CbcDec::new(key.as_bytes().into(), &iv.into())
+        .decrypt_padded_mut::<Pkcs7>(&mut buf)
+        .unwrap();
+    let b = String::from_utf8(pt.to_vec()).unwrap();
+    return b;
 }
 
 fn main() {
     let mut home_folder = dirs::home_dir().unwrap();
     home_folder.push(".passwords");
+    let mut passphrase_path = home_folder.clone();
+    passphrase_path.push("passphrase.txt");
     let args = PasswordManager::parse();
 
     match args.command {
         Commands::Init {} => {
-            create_dir(home_folder);
+            create_dir(home_folder.clone());
+            let passphrase = read_input("Enter a passphrase key");
+            fs::write(passphrase_path, passphrase)
+                .expect("Could not save passphrase please check permissions");
+            println!("Passphrase saved successfully");
         }
         Commands::Add { mut website } => {
             let path = create_password_file(&mut website, home_folder);
@@ -105,7 +140,19 @@ fn main() {
 
                 if password == password_retype {
                     println!("{path:?}");
-                    fs::write(path, password).expect("could not write to file");
+
+                    let mut passphrase_file =
+                        fs::File::open(passphrase_path).expect("Could not find passphrase path");
+                    let mut passphrase = String::new();
+                    passphrase_file
+                        .read_to_string(&mut passphrase)
+                        .expect("Could not read file, please check permissions");
+
+                    let encrypted = encrypt_password(&passphrase[..16], &password);
+
+                    let b64e = general_purpose::STANDARD.encode(encrypted.clone());
+
+                    fs::write(path, b64e).expect("could not write to file");
                     println!("Password Saved Sucessfully");
                 } else {
                     eprintln!("Passwords do not match!");
@@ -146,13 +193,25 @@ fn main() {
         Commands::Get { mut website } => {
             let path = create_password_file(&mut website, home_folder);
 
-            if let Ok(mut f) = fs::File::open(path) {
-                let mut contents = String::new();
-                f.read_to_string(&mut contents)
-                    .expect("Could not read the file");
-                println!("{}", contents);
+            if let Ok(mut p) = fs::File::open(passphrase_path) {
+                let mut passphrase = String::new();
+                p.read_to_string(&mut passphrase)
+                    .expect("Could not read passphrase");
+                if let Ok(mut f) = fs::File::open(path) {
+                    let mut contents = String::new();
+                    f.read_to_string(&mut contents)
+                        .expect("Could not read the file");
+
+                    let b64d = general_purpose::STANDARD
+                        .decode(contents)
+                        .expect("Could not convert b64 to string");
+                    let password = decrypt_password(b64d, &passphrase[..16]);
+                    println!("{:?}", password);
+                } else {
+                    eprintln!("Could not find the website");
+                }
             } else {
-                eprintln!("Could not find the website");
+                eprintln!("Could not find passphrase, please make sure to set one");
             }
         }
         Commands::List {} => {
